78ffa1780dcb732685cc7adabe89bac5d0ee7ef7
Mochamad Taufan Rezzafri <rezzafri@gmail.com>
2015-10-20 10:09:23 +0700

    Update config.

--- config/config.php
+++ config/config.php
@@ -5,6 +5,6 @@ return [
     'AWS_API_SECRET_KEY' => env('AWS_API_SECRET_KEY', ''),
     'AWS_ASSOCIATE_TAG' => env('AWS_ASSOCIATE_TAG', ''),
     'ENDPOINT' => env('AWS_ENDPOINT', 'com'),
-    'REQUEST' => '\ApaiIO\Request\Rest\Request',
-    'RESPONSE' => ''
+    'REQUEST' => env('AWS_REQUEST', '\ApaiIO\Request\Rest\Request'),
+    'RESPONSE' => env('AWS_RESPONSE', '')
 ];
